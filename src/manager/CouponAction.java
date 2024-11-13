b523cc68cd1024d5985defff85b83ca9d5636ca0
king <tky2302011@stu.o-hara.ac.jp>
2024-11-13 10:41:19 +0900

    クーポン更新

---
+++ src/manager/CouponAction.java
@@ -0,0 +1,46 @@
+package manager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import DAO.CouponDAO;
+import been Coupon
+
+public class LoginAction extends Action {
+
+	public void execute(
+	HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+
+		HttpSession session=request.getSession();
+
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		UserDAO dao=new UserDAO();
+		User user = dao.search(email, password);
+
+
+
+		if(user!= null){
+			session.setAttribute("user", user);
+			if(user.getFlag().equals("2")){
+				request.getRequestDispatcher("../staff/StaffHome.jsp").forward(request, response);
+
+			}
+
+
+				else if(user.getFlag().equals("1")){
+
+
+				request.getRequestDispatcher("../suizokutachiproject/home/home J.jsp").forward(request, response);
+
+				}
+
+			}
+
+		}
+
+		//login
+	}
