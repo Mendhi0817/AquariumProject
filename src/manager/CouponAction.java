91c24ff26cc4046147bbae68f8a4b240f220621e
king <tky2302011@stu.o-hara.ac.jp>
2024-11-14 09:34:37 +0900

    d

--- src/manager/CouponAction.java
+++ src/manager/CouponAction.java
@@ -2,45 +2,19 @@ package manager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import DAO.CouponDAO;
-import been Coupon
+import tool.Action;
 
-public class LoginAction extends Action {
+
+public class CouponAction extends Action {
 
 	public void execute(
 	HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
 
+		request.getRequestDispatcher("../staff/Coupon/coupon_make.jsp").forward(request, response);
 
-		HttpSession session=request.getSession();
-
-		String email=request.getParameter("email");
-		String password=request.getParameter("password");
-		UserDAO dao=new UserDAO();
-		User user = dao.search(email, password);
-
-
-
-		if(user!= null){
-			session.setAttribute("user", user);
-			if(user.getFlag().equals("2")){
-				request.getRequestDispatcher("../staff/StaffHome.jsp").forward(request, response);
-
-			}
-
-
-				else if(user.getFlag().equals("1")){
-
-
-				request.getRequestDispatcher("../suizokutachiproject/home/home J.jsp").forward(request, response);
-
-				}
-
-			}
-
-		}
+	}
 
 		//login
 	}
