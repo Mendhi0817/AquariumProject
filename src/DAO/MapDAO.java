10dae7be29f16cac116f8fd172d3ff343523db2d
華綸 <tky2302095@stu.o-hara.ac.jp>
2024-11-15 10:44:56 +0900

    map上げ直し

--- src/DAO/MapDAO.java
+++ src/DAO/MapDAO.java
@@ -3,42 +3,78 @@ package DAO;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import bean.Map;
 public class MapDAO extends DAO {
 
-	public Map search(String image, int floorinfo,int mapid)
-			throws Exception {
+	public Map search(String image, int floor) throws Exception {
 		Map map = null;
-
 		Connection con = getConnection();
-
 		PreparedStatement st;
-		st = con.prepareStatement("select * from userinfo where image = ? and floor=?");
-		st.setString(1,image);
-		st.setInt(2,floorinfo);
-		st.setInt(3, mapid);
 
+		st = con.prepareStatement("select * from mapinfo where map_image = ? and floor_info=?");
+		st.setString(1,image);
+		st.setInt(2,floor);
 
 		ResultSet rs = st.executeQuery();
 
 		while (rs.next()) {
 			map = new Map();
 			map.setMapImage(rs.getString("image"));
-			map.setFloorInfo(rs.getInt("floorinfo"));
-			map.setMapId(rs.getInt("mapid"));
+			map.setFloorInfo(rs.getInt("floor"));
+
 		}
 		st.close();
 		con.close();
 		return map;
 
-
-
 	}
 
 
+public boolean save(Map map) throws Exception {
+	// コネクションを確立
+	Connection con = getConnection();
+	// プリペアードステートメント
+	PreparedStatement st = null;
 
+	String image = null;
+	int floor = 0;
 
+	// 実行件数
+	int count = 0;
 
+	try {
+		if (image == null) {
+			// 存在しなかった場合
+			// プリペアードステートメントにINSERT文をセット
+		    st = con.prepareStatement("insert into MAPINFO(MAP_IMAGE, FLOOR_INFO) values(?, ?)");
+			st.setString(1, map.getMapImage());
+			st.setInt(2, map.getFloorInfo());
 
+		}
+		// プリペアードステートメントを実行
+		count = st.executeUpdate();
+
+	} catch (Exception e) {
+		throw e;
+	} finally {
+		// プリペアードステートメントを閉じる
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException sqle) {
+				throw sqle;
+			}
+		}
+	}
+
+	if (count > 0) {
+		// 実行件数が1件以上ある場合
+		return true;
+	} else {
+		//実行件数が0件の場合
+		return false;
+	}
+	}
 }
--- src/bean/Map.java
+++ src/bean/Map.java
@@ -6,8 +6,6 @@ public class Map {
 
 	private int FloorInfo;
 
-	private int MapId;
-
 	public String getMapImage() {
 		return MapImage;
 	}
@@ -24,94 +22,4 @@ public class Map {
 		this.FloorInfo = floorInfo;
 	}
 
-	public int getMapId() {
-		return MapId;
-	}
-
-	public void setMapId(int mapId) {
-		this.MapId = mapId;
-	}
-
-
-
-
 }
-
-/* package bean;
-
-public class Student {
-
-   // 学生番号
-   private String no;
-   // 学生名
-   private String name;
-   // 入学年度
-   private int entYear;
-   // クラス番号
-   private String classNum;
-   // 出席状況
-   private boolean isAttend;
-   // 所属する学校
-   private School school;
-
-
-
-   // 学生番号のゲッターとセッター
-   public String getNo() {
-       return no;
-   }
-   public void setNo(String no) {
-       this.no = no;
-   }
-
-
-
-   // 学生名のゲッターとセッター
-   public String getName() {
-       return name;
-   }
-   public void setName(String name) {
-       this.name = name;
-   }
-
-
-
-   // 入学年度のゲッターとセッター
-   public int getEntYear() {
-       return entYear;
-   }
-   public void setEntYear(int entYear) {
-       this.entYear = entYear;
-   }
-
-
-
-   // クラス番号のゲッターとセッター
-   public String getClassNum() {
-       return classNum ;
-   }
-   public void setClassNum(String classNum) {
-       this.classNum = classNum;
-   }
-
-
-
-   // 出席状況のゲッターとセッター
-   public boolean isAttend() {
-       return isAttend;
-   }
-   public void setAttend(boolean isAttend) {
-       this.isAttend = isAttend;
-   }
-
-
-
-   // 所属する学校のゲッターとセッター
-   public School getSchool() {
-       return school;
-   }
-   public void setSchool(School school) {
-       this.school = school;
-   }
-}
-*/
