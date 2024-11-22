c3a78eeb9c8461eff682ca2688ec2ecb02918d96
TATowoomo <tky2302125@stu.o-hara.ac.jp>
2024-11-22 12:35:42 +0900

    更新しました

--- src/manager/Notification_data_PostAction.java
+++ src/manager/Notification_data_PostAction.java
@@ -1,14 +1,11 @@
 package manager;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import DAO.NotificationDataDAO;
-import bean.NotificationData;
 import tool.Action;
 
 public class Notification_data_PostAction extends Action {
@@ -17,19 +14,7 @@ public class Notification_data_PostAction extends Action {
 
         try {
 
-            NotificationDataDAO nDAO = new NotificationDataDAO();
 
-            List<NotificationData> listTitle = nDAO.searchTitle();
-            List<NotificationData> listId = nDAO.searchId();
-
-
-            // 取得したデータをリクエスト属性にセット
-
-            request.setAttribute("listTitle", listTitle);
-            request.setAttribute("listId", listId);
-
-
-            // JSPにフォワード
 
 			request.getRequestDispatcher("../staff/notification_data/notification_data.jsp").forward(request, response);
 
