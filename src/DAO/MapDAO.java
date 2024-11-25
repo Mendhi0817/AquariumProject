420436863dedb75ba81f5a5842f283fafa010914
華綸 <tky2302095@stu.o-hara.ac.jp>
2024-11-25 12:18:01 +0900

    MapDelete表示成功

--- src/DAO/MapDAO.java
+++ src/DAO/MapDAO.java
@@ -15,7 +15,7 @@ public class MapDAO extends DAO {
 		Connection con = getConnection();
 		PreparedStatement st;
 
-		st = con.prepareStatement("select * from mapinfo");
+		st = con.prepareStatement("select * from map_info");
 //		st.setString(1,image);
 
 
