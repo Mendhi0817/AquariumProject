18850315e907b93ea0fed386d490cae7e3eac530
'djangouser' <'tky2302008@stu.o-hara.ac.jp'>
2024-11-26 15:04:29 +0900

    hfh

--- src/manager/Notification_data_customerAction.java
+++ src/manager/Notification_data_customerAction.java
@@ -1,8 +1,13 @@
 package manager;
 
+import java.util.List;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.NotificationDataDAO;
+import bean.NotificationData;
 import tool.Action;
 
 
@@ -12,9 +17,46 @@ public class Notification_data_customerAction extends Action {
 	HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
 
+		try {
+//			// ローカル変数の宣言
+//
+//			HttpSession session = req.getSession(); // セッション情報を取得
+//			PreparedStatement statement = null;
+//
+//
+////			NotificationData notification = (NotificationData)session.getAttribute("user");
+//
+//
+//
+//			Contuct contact = cDao.get();
+//
+//			req.setAttribute("title", contact.getContactTitle());	// リクエストにデータをセット
+//
+            NotificationDataDAO nDAO = new NotificationDataDAO();
+
+            List<NotificationData> listTitle = nDAO.searchTitle();
+            List<NotificationData> listId = nDAO.searchId();
+
+
+            // 取得したデータをリクエスト属性にセット
+
+            request.setAttribute("listTitle", listTitle);
+            request.setAttribute("listId", listId);
+
+
 		request.getRequestDispatcher("../staff/notification_data/notification_data_customer.jsp").forward(request, response);
 
+	} catch (Exception e) {
+
+        throw new ServletException(e);
+
 	}
+//		req.getRequestDispatcher("../staff/notification_data/notification_data_edit.jsp").forward(request, response);
+
+    }
+
+}
+
+
 
 		//login
-	}
