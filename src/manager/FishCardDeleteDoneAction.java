90aa1d9ab95e40215150bf6be23854db85705fb1
華綸 <tky2302095@stu.o-hara.ac.jp>
2024-11-29 10:29:04 +0900

    aaa

--- src/manager/FishCardDeleteDoneAction.java
+++ src/manager/FishCardDeleteDoneAction.java
@@ -3,18 +3,25 @@ package manager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import DAO.MapDAO;
 import tool.Action;
-
 
 public class FishCardDeleteDoneAction extends Action {
 
-	public void execute(
-	HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+//        HttpSession session=req.getSession();
 
-		request.getRequestDispatcher("../staff/Card/fish_card_delete_done.jsp").forward(request, response);
+        // 削除対象のカードIDを取得
+        String floorInfo = req.getParameter("mapId");
 
-	}
 
-		//login
-	}
+
+        // FishCardDAO を使用してデータベースから削除
+        MapDAO mapDAO = new MapDAO();
+        mapDAO.delete(floorInfo);
+
+
+            req.getRequestDispatcher("../staff/Card/fish_card_delete_done.jsp").forward(req, res);
+    }
+}
