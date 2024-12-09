4fe1a09ad299b1727bc950a21e046119b4129b87
mendhi <tky2302115@stu.o-hara.ac.jp>
2024-12-09 15:17:09 +0900

    途中

--- src/manager/CameraAction.java
+++ src/manager/CameraAction.java
@@ -2,8 +2,11 @@ package manager;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import DAO.FishCardDAO;
 import bean.Camera;
+import bean.FishCard;
 import tool.Action;
 
 public class CameraAction extends Action {
@@ -14,6 +17,15 @@ public class CameraAction extends Action {
 
 		System.out.println("読み取り後");
 		System.out.println(camera.path);
+
+
+		HttpSession session=request.getSession();
+		FishCardDAO fishcarddao=new FishCardDAO();
+		FishCard fishcard = fishcarddao.search(id);
+
+
+
+
 
 
 
